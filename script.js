e1117f0f72cb6b9720464d2e6bcb4789b8d94e8b
Luciano dos Santos <110494736+LudsnNeto@users.noreply.github.com>
2022-11-26 12:23:49 -0300

    f

--- script.js
+++ script.js
@@ -3,7 +3,11 @@
 var controleCampo = 1;
 function adicionarCampo(){
     controleCampo++;
-    document.getElementById('selectIten').insertAdjacentHTML('beforeend','<div '+controleCampo+'><form id="selectIten"> <input type="text " name="produtos" id="product"> <button type="button" onclick="adicionarCampo()" id="btn"> + </button></form> <button type="button" id"'+controleCampo+'" onclick="removerCampo('+controleCampo+')">-</button> </div>');
+    document.getElementById('selectIten').insertAdjacentHTML('beforeend','<div id="form'+controleCampo+'"><form  id="campo ' + controleCampo + '" id="selectIten"> <input type="text " name="produtos" id="product"> <button type="button" onclick="adicionarCampo()" id="btn"> + </button></form> <button type="button" id"'+controleCampo+'" onclick="removerCampo('+controleCampo+')".remove>-</button> </div>');
+}
+
+function removerCampo(idCampo){
+    document.getElementById('campo' + idCampo).remove();
 }
 
 
