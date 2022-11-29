7f8ce69fc295d48e22d346fa4e2362647fc55d94
Luciano dos Santos <110494736+LudsnNeto@users.noreply.github.com>
2022-11-29 15:24:05 -0300

    purchaselist 1.0

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
-
 var controleCampo = 1;
-function adicionarCampo(){
+function adicionarCampo() {
     controleCampo++;
-    document.getElementById('selectIten').insertAdjacentHTML('beforeend','<div id="form'+controleCampo+'"><form  id="campo ' + controleCampo + '" id="selectIten"> <input type="text " name="produtos" id="product"> <button type="button" onclick="adicionarCampo()" id="btn"> + </button></form> <button type="button" id"'+controleCampo+'" onclick="removerCampo('+controleCampo+')".remove>-</button> </div>');
+    document.getElementById('selectIten').insertAdjacentHTML('beforeend','<div id="campo' + controleCampo + '"><form'
+        + controleCampo + '" id="selectIten"> <input type="text " name="produtos" id="product"> </button></form><button type="button"' + controleCampo + ' onclick="removerCampo(' + controleCampo + ')".remove> - </button> </div>');
 }
 
-function removerCampo(idCampo){
+function removerCampo(idCampo) {
     document.getElementById('campo' + idCampo).remove();
 }
-
-
 
